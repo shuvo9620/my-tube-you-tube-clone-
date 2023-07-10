77f8441f1fc98e5c3631f61762255a249a3a3b4a
shuvo9620 <smahfuzur0@gmail.com>
2023-07-11 00:26:56 +0600

    search bar added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,23 +31,48 @@ const Header = () => {
     const pageName = pathname?.split("/")?.filter(Boolean)?.[0];
 
     return (
-        <div className='sticky top-0 z-10 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-white dark:bg-black'>
-            {loading && <Loader></Loader>}
+        <div className="sticky top-0 z-10 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-black dark:bg-black">
+            {loading && <Loader />}
+
             <div className="flex h-5 items-center">
                 {pageName !== "video" && (
-                    <div className='flex md:hidden md:mr-6 cursor-pointer items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]' onClick={mobileMenuToggle}
+                    <div
+                        className="flex md:hidden md:mr-6 cursor-pointer items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]"
+                        onClick={mobileMenuToggle}
                     >
                         {mobileMenu ? (
-                            <CgClose className='text-xl text-white'></CgClose>
+                            <CgClose className="text-white text-xl" />
                         ) : (
-                            <SlMenu className='text-xl text-white'></SlMenu>
+                            <SlMenu className="text-white text-xl" />
                         )}
-
                     </div>
                 )}
-                
-                <img className='h-full hidden dark:md:block' src={ytLogo} alt="logo" />
-                <img className='h-full md:hidden' src={ytLogoMobile} alt="logoMobile" />
+                <Link to="/" className="flex h-5 items-center">
+                    <img
+                        className="h-full hidden lg:block md:block dark:md:block"
+                        src={ytLogo}
+                        alt="Youtube"
+                    />
+                    <img
+                        className="h-full md:hidden"
+                        src={ytLogoMobile}
+                        alt="Youtube"
+                    />
+                </Link>
+                <div className="group flex items-center">
+                    <div className="flex h-8 md:h-10 md:ml-10 md:pl-5 border border-[#303030] rounded-l-3xl group-focus-within:border-blue-500 md:group-focus-within:ml-5 md:group-focus-within:pl-0">
+                        <div className='w-10 items-center justify-center hidden group-focus-within:md:flex'>
+                            <IoIosSearch className='text-white text-xl' />
+                        </div>
+                        <input
+                            type="text"
+                            className='bg-transparent outline-none text-white pr-5 pl-5 md:pl-0 w-44 md:group-focus-within:pl-0 md:w-64 lg:w-[500px]'
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                            onKeyUp={searchQueryHandler}
+                            value={searchQuery}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     );
