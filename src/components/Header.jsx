5b88b0da0ce5d7e81a5b893be44a07fdba44b54c
shuvo9620 <smahfuzur0@gmail.com>
2023-07-10 22:16:28 +0600

    handler added for search query in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
+import {Link, useLocation, useNavigate} from 'react-router-dom';
+import ytLogo from "../assets/images/yt-logo.png";
+import ytLogoMobile from "../assets/images/yt-logo-mobile.png";
+import {SlMenu} from "react-icons/sl";
+import {IoIosSearch} from "react-icons/io";
+import {RiVideoAddLine} from "react-icons/ri";
+import {FiBell} from "react-icons/fi";
+import {CgClose} from "react-icons/cg";
+import { Context } from '../context/contextApi';
+import Loader from "../shared/loader";
 
 const Header = () => {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const {loading, mobileMenu, setMobileMenu} = useContext(Context)
+
+    const navigate = useNavigate();
+
+    const searchQueryHandler = (event) => {
+        if(event.key === "Enter" ||event === "searchButton") {
+    }
     return (
         <div>
             <h2>header</h2>
