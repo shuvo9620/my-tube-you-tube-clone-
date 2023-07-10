7dc82912e1c1ebf33353aa5ab9374d8332b4ca58
shuvo9620 <smahfuzur0@gmail.com>
2023-07-10 22:38:10 +0600

    loader component added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,9 +31,7 @@ const Header = () => {
     const pageName = pathname?.split("/")?.filter(Boolean)?.[0];
 
     return (
-        <div>
-            <h2>header</h2>
-        </div>
+        <div className='sticky top-0 z-10 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-white dark:bg-black'></div>
     );
 };
 
