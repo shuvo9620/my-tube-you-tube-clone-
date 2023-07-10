e6aa747b458a2cf6b64c29046e00e7fd9e177fc2
shuvo9620 <smahfuzur0@gmail.com>
2023-07-10 21:56:49 +0600

    route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AppContext } from './context/contextApi';
+import Header from './components/Header';
+import Feed from './components/Feed';
+import SearchResult from './components/SearchResult';
+import VideoDetails from './components/VideoDetails';
+
 
 const App = () => {
   return (
     <AppContext>
-      <h2 className='text-red-600'>shuvo</h2>
+      <BrowserRouter>
+        <div className='flex flex-col h-full'>
+          <Header></Header>
+          <Routes>
+            <Route path='/' element={<Feed></Feed>}></Route>
+            <Route path='/searchResult/:searchQuery' element={<SearchResult></SearchResult>}></Route>
+            <Route path='/video/:id' element={<VideoDetails></VideoDetails>}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </AppContext>
   );
 };
