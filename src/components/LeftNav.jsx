2114c55c88be5b4718d324cc0b72c27dc34d12db
shuvo9620 <smahfuzur0@gmail.com>
2023-07-12 01:20:30 +0600

    categories data load resolved

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -6,18 +6,17 @@ import { categories } from "../utils/constants";
 import { Context } from "../context/contextApi";
 
 const LeftNav = () => {
-    const { selectedCategory, setSelectedCategory, mobileMenu } = useContext(Context);
+    const { selectCategories, setSelectCategories, mobileMenu } =
+        useContext(Context);
 
     const navigate = useNavigate();
 
     const clickHandler = (name, type) => {
         switch (type) {
             case "category":
-                setSelectedCategory(name);
-                break;
+                return setSelectCategories(name);
             case "home":
-                setSelectedCategory(name);
-                break;
+                return setSelectCategories(name);
             case "menu":
                 return false;
             default:
@@ -32,20 +31,28 @@ const LeftNav = () => {
             }`}
         >
             <div className="flex px-5 flex-col">
-                {categories.map((item) => (
-                    <React.Fragment key={item.name}>
-                        <LeftNavMenuItem
-                            text={item.type === "home" ? "Home" : item.name}
-                            icon={item.icon}
-                            action={() => {
-                                clickHandler(item.name, item.type);
-                                navigate("/");
-                            }}
-                            className={selectedCategory === item.name ? "bg-white/[0.15]" : ""}
-                        />
-                        {item.divider && <hr className="my-5 border-white/[0.2]" />}
-                    </React.Fragment>
-                ))}
+                {categories.map((item) => {
+                    return (
+                        <React.Fragment key={item.name}>
+                            <LeftNavMenuItem
+                                text={item.type === "home" ? "Home" : item.name}
+                                icon={item.icon}
+                                action={() => {
+                                    clickHandler(item.name, item.type);
+                                    navigate("/");
+                                }}
+                                className={`${
+                                    selectCategories === item.name
+                                        ? "bg-white/[0.15]"
+                                        : ""
+                                }`}
+                            />
+                            {item.divider && (
+                                <hr className="my-5 border-white/[0.2]" />
+                            )}
+                        </React.Fragment>
+                    );
+                })}
                 <hr className="my-5 border-white/[0.2]" />
                 <div className="text-white/[0.5] text-[12px]">
                     <a href="https://web.facebook.com/mahfuzur.shuvo">Clone by: Mahfuzur Shuvo</a>
