12f3135ddae488406721ae3eda99ad53fd2d1865
shuvo9620 <smahfuzur0@gmail.com>
2023-07-11 23:31:27 +0600

    showing video length using moment js

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { abbreviateNumber } from "js-abbreviation-number";
 import { Link } from "react-router-dom";
 import { BsFillCheckCircleFill } from "react-icons/bs";
-
 import VideoLength from "../shared/videoLength";
 
 const VideoCard = ({ video }) => {
--- src/shared/videoLength.jsx
+++ src/shared/videoLength.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import moment from "moment";
 
-const videoLength = () => {
+const VideoLength = ({ time }) => {
+    const videoLengthInSeconds = moment()
+        ?.startOf("day")
+        ?.seconds(time)
+        ?.format("H:mm:ss");
     return (
-        <div>
-            
-        </div>
+        <span className="absolute bottom-2 right-2 bg-black py-1 px-2 text-white text-xs rounded-md">
+            {videoLengthInSeconds}
+        </span>
     );
 };
 
-export default videoLength;
+export default VideoLength;
